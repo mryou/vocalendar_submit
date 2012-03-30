78fe7cb2e75e52bf7211bfaba42cbe98445f2aea
Tatsuki Sugiura <sugi@nemui.org>
2012-03-31 02:40:57 +0900

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VocalendarSubmit::Application.config.secret_token = 'cee49369fa6fff36bc852e45e2c495f521d8c9fde4a643764e61ec6aa32236ee6432b5c8afe61d0a15cf4680752248822b4312bf25608a62c958f0e32f055027'
