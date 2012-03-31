e6d702c1f1392f29d00685b4bb9022ba5bb8eb4b
Tatsuki Sugiura <sugi@nemui.org>
2012-04-01 03:46:23 +0900

    minor style fix.

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module VocalendarSubmit
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -55,8 +56,6 @@ module VocalendarSubmit
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.autoload_paths += %W(#{config.root}/lib)
 
     config.i18n.default_locale = :ja
   end
