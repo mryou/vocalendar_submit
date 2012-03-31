1df6b936825e7806391a3686acc8b0d507e2a4db
Tatsuki Sugiura <sugi@nemui.org>
2012-04-01 04:20:04 +0900

    add google calendar event add link button.

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,22 @@
+# -*- encoding: utf-8 -*-
 module SubmissionsHelper
+  def gcal_add_button(sub)
+    urlbase = 'http://www.google.com/calendar/event?action=TEMPLATE&'
+    attrs = {
+      :text => sub.title,
+    }
+    if sub.all_day?
+      attrs[:dates] = sub.start_datetime.strftime("%Y%m%d") +
+        '/' + (sub.end_datetime + 1.day).strftime("%Y%m%d")
+      # why do google need +1 day...
+    else
+      fmt = "%Y%m%dT%H%M00Z"
+      attrs[:dates] = sub.start_datetime.to_datetime.new_offset(0).strftime(fmt) +
+        '/' + sub.end_datetime.to_datetime.new_offset(0).strftime(fmt)
+    end
+    sub.where.blank? or  attrs[:location] = sub.where
+    sub.description.blank? or  attrs[:details] = sub.description
+    url = urlbase + attrs.map{|k, v| "#{k}=#{CGI.escape(v)}"}.join('&')
+    link_to 'Googleカレンダーの編集画面を開く', url, :class => 'action-button main-action add-google-cal', :target => '_blank'
+  end
 end
--- lib/nemui/ar_utils.rb
+++ lib/nemui/ar_utils.rb
@@ -6,13 +6,4 @@ module Nemui::ArUtils
     obj.respond_to?(:id) and return "#{obj.class.name}#{obj.id}"
   end
 
-  def ar_t(obj, key)
-    if obj.is_a? ActiveRecord::Base 
-      t("activerecord.attributes.#{obj.class.name.underscore}.#{key}")
-    elsif obj.is_a? ActionView::Helpers::FormBuilder
-      t("activerecord.attributes.#{obj.object_name}.#{key}")
-    else
-      t("activerecord.attributes.#{obj.to_s.underscore}.#{key}")
-    end
-  end
 end
