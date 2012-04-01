4a2c9985268b6cb0aa285ddd02e1d9c52a8faadb
Tatsuki Sugiura <sugi@nemui.org>
2012-04-01 22:01:13 +0900

    add adhoc-menu and submission full listing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 VocalendarSubmit::Application.routes.draw do
-  resources :submissions
+  resources :submissions do
+    collection do
+      get 'list' => 'submissions#list'
+    end
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
