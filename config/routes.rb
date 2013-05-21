fd09cfca7aa29c050fc803ed95c34ea9610bf3e2
mryou <ryou@maple.dti2.ne.jp>
2013-05-21 17:44:17 +0900

    ディフォルト画面を登録画面に変更

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ VocalendarSubmit::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'submissions#index'
+  root :to => 'submissions#new'
 
   # See how all your routes lay out with "rake routes"
 
